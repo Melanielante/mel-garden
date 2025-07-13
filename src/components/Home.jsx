d5dd97dc6c99b55dee98e6a3784c444b9609a717
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-07-14 01:23:01 +0300

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function Home() {
+  return (
+    <div>
+        <h2>Welcome to Mel Garden</h2>
+        <p>This is a space where you grow your react skills, one plant at a time.</p>
+    </div>
+  )
+}
+
+export default Home;
