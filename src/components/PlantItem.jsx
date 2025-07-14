4fb6dca7c27d0f7baf2375882bde0e3e1235ffed
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-07-14 04:37:21 +0300

    states and events

--- src/components/PlantItem.jsx
+++ src/components/PlantItem.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react';
+
 
 function PlantItem({ name, type, description, image, isIndoor }) {
+  const [likes, setLikes] = useState(0);
+  
+  function handleLikeClick() {
+    setLikes(likes + 1);
+  }
+  
   return (
     <div className='plant-card'>
         <h3>{name}</h3>
@@ -14,6 +21,7 @@ function PlantItem({ name, type, description, image, isIndoor }) {
             ? <span style={{backgroundColor: "lightgreen", padding: "4px", borderRadius: "4px"}}>Indoor Plant</span>
             : <span style={{backgroundColor: "lightyellow", padding: "4px", borderRadius: "4px"}}>Outdoor Plant</span>
         }
+        <button onClick={handleLikeClick}>{likes}</button>
         
     </div>
   )
